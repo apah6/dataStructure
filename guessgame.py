a26232fd5109f5d3646a098d0b8ccafe73a0c4da
apah6 <meng9940@naver.com>
2025-03-24 10:46:59 +0900

    v0.3 array

--- guessgame.py
+++ guessgame.py
@@ -4,12 +4,12 @@ answer_number = random.randrange(1,101)
 guess_count = 0
 is_guessing = True
 print("즐거운 숫자 맞추기~~")
-print(answer_number)
+#print(answer_number)
 
 while is_guessing:
 
     guess_count += 1
-    if guess_count == 6:
+    if guess_count == 7:
         print("정답을 맞추지 못햇습니다.")
         break
     guess_number = int(input(f"{7 - guess_count}번의 기회가 남았습니다. 숫자를 입력: "))
