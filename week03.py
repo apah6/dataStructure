9ab78b2a039937eccf5913f968eef1ee76368c0c
apah6 <meng9940@naver.com>
2025-03-24 11:55:35 +0900

    v0.7 array

--- week03.py
+++ week03.py
@@ -1,5 +1,18 @@
-groups = ['HOT','BlackPink','SevenTeen','NJZ']
-rating = [1,2,3,4]
+def duplicate_city(cities):
+    result = list()
+    s = set()
 
-group_rating = list(zip(groups, rating))
-print(group_rating)
+    for city in cities:
+        l1 = len(s)
+        s.add(city)
+        l2 = len(s)
+        if l1 == l2:
+            result.append(city)
+    return result
+cities = ['Incheon','Incheon','Incheon','Gimpo','Seoul','Seoul']
+
+cities.append('Anayang')
+cities.append('Seoul')
+
+print(cities)
+print(set(duplicate_city(cities)))
