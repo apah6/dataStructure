5073c45d9801d18be750d5317c11964574ed3d70
apah6 <meng9940@naver.com>
2025-05-19 11:57:45 +0900

    V2.9 graph

--- week11.py
+++ week11.py
@@ -1,44 +1,21 @@
-class Graph:
-    def __init__(self, size):
-        self.SIZE = size
-        self.graph = [[0 for _ in range(size)] for _ in range(size)]
+graph = [
+    [0, 1, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 0, 1, 1, 1, 0],
+    [0, 0, 0, 1, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 1, 0]
+]
 
+#깊이 우선 탐색
+def dfs(g, i, visited):
+    visited[i] = 1
+    print(chr(ord('A')+i), end =' ') #방문 노드 출력
+    for j in range(len(g)):
+        if g[i][j] == 1 and not visited[j]:
+            dfs(g, j, visited)
 
-G1 = Graph(4)
-G2 = Graph(4)
-G_self = Graph(4)
-
-# 0 == A, 1 == B, 2 == C, 3 == D
-G1.graph[0][1] = 1; G1.graph[0][2] = 1; G1.graph[0][3] = 1
-G1.graph[1][0] = 1; G1.graph[1][2] = 1
-G1.graph[2][0] = 1; G1.graph[2][1] = 1; G1.graph[2][3] = 1
-G1.graph[3][0] = 1; G1.graph[3][2] = 1
-
-print("G1 무방향 그래프")
-for r in range(G1.SIZE):
-    for c in range(G1.SIZE):
-        print(G1.graph[r][c], end=' ')
-    print()
-
-
-# 0 == A, 1 == B, 2 == C, 3 == D
-G2.graph[0][1] = 1; G2.graph[0][2] = 1
-G2.graph[3][0] = 1; G2.graph[3][2] = 1
-
-print("G2 방향 그래프")
-for r in range(G2.SIZE):
-    for c in range(G2.SIZE):
-        print(G2.graph[r][c], end=' ')
-    print()
-
-# 0 == A, 1 == B, 2 == C, 3 == D
-G_self.graph[0][3] = 1
-G_self.graph[1][2] = 1; G_self.graph[1][3] = 1
-G_self.graph[2][1] = 1
-G_self.graph[3][0] = 1; G_self.graph[3][1] = 1
-
-print("G_self 무방향 그래프")
-for r in range(G_self.SIZE):
-    for c in range(G_self.SIZE):
-        print(G_self.graph[r][c], end=' ')
-    print()
+visited_dfs = [0 for _ in range(len(graph))]
+dfs(graph, 7, visited_dfs)
