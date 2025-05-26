d4000031c0e56afb3376aad042a8523a4b55e488
apah6 <meng9940@naver.com>
2025-05-26 10:17:49 +0900

    V3.0 deque

--- week11.py
+++ week11.py
@@ -17,5 +17,19 @@ def dfs(g, i, visited):
         if g[i][j] == 1 and not visited[j]:
             dfs(g, j, visited)
 
+#너비 우선 탐색
+def bfs(g, i, visited):
+    visited[i] = 1
+    print(chr(ord('A') + i), end=' ')
+    for j in range(len(g)):
+        if not visited[j]:
+            visited[j] = 1
+            print(chr(ord('A') + j), end = ' ')
+        pass
+
 visited_dfs = [0 for _ in range(len(graph))]
+visited_bfs = [0 for _ in range(len(graph))]
+
 dfs(graph, 7, visited_dfs)
+print()
+bfs(graph, 4, visited_bfs) #GDBHCEFA
